643d3600fe4181510f36249c2d0d9301b9caa425
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-02-17 00:51:46 +0530

    Navbar Completed

--- sandaruwan/src/App.jsx
+++ sandaruwan/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
      
-     hello
+     <Navbar/>
     </>
   )
 }
---
+++ sandaruwan/src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div className="container m-auto px-4 py-6">
+            <div className="flex justify-between items-center" >
+                <div>
+                    <h1 className="font-bold text-xl">Sandaruwan Warnasooriya</h1>
+                </div>
+
+                <div>
+                    <ul className="flex gap-5">
+                        <li>
+                            <a href="" className="text-gray-400 hover:text-white cursor-pointer">Projects</a>
+                        </li>
+                        <li>
+                            <a href="" className="text-gray-400 hover:text-white cursor-pointer">Technologies</a>
+                        </li>
+                        <li>
+                            <a href="" className="text-gray-400 hover:text-white cursor-pointer">About Me</a>
+                        </li>
+                        <li>
+                            <a href="" className="text-gray-400 hover:text-white cursor-pointer">Contact Me</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+      
+    </header>
+  )
+}
+
+export default Navbar
